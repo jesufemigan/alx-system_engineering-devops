e43e1333fb16268c94af37cfcc823dbb663930bf
Jesufemi <oladapofemmy@gmail.com>
2024-03-22 20:47:35 +0100

    fix 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # a puppet manifest that installs flask
-package { 'flask':
-  ensure  => '2.1.0',
-  name    => flask,
-  command => pip3 install flask,
+package { 'Flask':
+  ensure   => '2.1.0',
+  name     => flask,
+  command  => flask,
+  provider => pip3
 }
