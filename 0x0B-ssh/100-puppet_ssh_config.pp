80641d985adb4bd892bc244d1fdc3e588f5a072c
Jesufemi <oladapofemmy@gmail.com>
2024-03-23 02:38:27 +0100

    f100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,10 +4,10 @@ file { '/etc/ssh/ssh_config':
   ensure => present
 }
 
-# file_line { 'remove password auth':
-#  path => '/etc/ssh/ssh_config',
-#  line => 'PasswordAuthentication no'
-# }
+file_line { 'remove password auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
+}
 
 file_line { 'add file identity':
   path => '/etc/ssh/ssh_config',
