bf71c956904342cfaef8088510a57bb6ae67e9ae
Jesufemi <oladapofemmy@gmail.com>
2024-03-26 21:12:28 +0100

    puppet file

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# set up new ubuntu server with nginx
+
+exec { 'update system': 
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure   => installed,
+  require  => Exec['update system']
+}
+
+file { '/var/www/html/index.html':
+  content   => 'Hello World!'
+}
+
+exec { 'redirect_me': 
+  command   => 'sed -i "24i\        rewrite ^/redirect_me https://ww.youtube.com/watch?vQH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default,
+  provider  => shell
+}
+
+service { 'nginx': 
+  ensure  => running,
+  require => Package['nginx']
+} 
