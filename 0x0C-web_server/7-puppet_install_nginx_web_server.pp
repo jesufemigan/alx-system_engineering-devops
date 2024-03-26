82698e8dc2919c15bd6e4bb9f2cf5c13c55d544b
Jesufemi <oladapofemmy@gmail.com>
2024-03-26 21:28:11 +0100

    update 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,7 @@
 # set up new ubuntu server with nginx
 
 exec { 'update system': 
-  command => '/usr/bin/apt-get update',
+  command => '/usr/bin/apt-get -y update',
 }
 
 package { 'nginx':
@@ -9,16 +9,37 @@ package { 'nginx':
   require  => Exec['update system']
 }
 
-file { '/var/www/html/index.html':
-  content   => 'Hello World!'
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
+}
+
+exec { 'allow_http':
+  command  => 'usr/bin/ufw allow "Nginx HTTP"
+}
+
+file { '/var/www/html':
+  ensure  => 'directory',
+  owner   => $::id,
+  group   => $::id,
+  recurse => '0755'
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure   => 'file',
+  content  => 'Hello World!'
 }
 
 exec { 'redirect_me': 
-  command   => 'sed -i "24i\        rewrite ^/redirect_me https://ww.youtube.com/watch?vQH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default,
-  provider  => 'shell'
+  command   => 'sed -i "24i\        rewrite ^/redirect_me https://ww.youtube.com/watch?vQH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider  => 'shell',
+  require   => Package['nginx'],
+  notify    => Service['nginx']
 }
 
 service { 'nginx': 
-  ensure  => running,
-  require => Package['nginx']
+  ensure    => 'running',
+  require   => Package['nginx'],
+  subscribe => Exec['redirect_me']
 } 
