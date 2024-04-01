75208f8880a7fb3b979505617f2fcdf4c291f39d
Jesufemi <oladapofemmy@gmail.com>
2024-04-01 08:39:45 +0100

    task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+exec { 'HTTP header':
+  command  => 'sudo sed -i "24i\        add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  provider => shell
+}
+
+exec { 'restart nginx':
+  command => 'sudo service nginx restart',
+ }
