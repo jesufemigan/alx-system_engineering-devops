c1fe0e9abb1d799de134151656bc9457741454e1
Jesufemi <oladapofemmy@gmail.com>
2024-04-22 20:22:27 +0100

    fix0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,19 +4,20 @@
 import requests
 import sys
 
-employee_id = sys.argv[1]
-user_url = 'https://jsonplaceholder.typicode.com/users'
-todo_url = 'https://jsonplaceholder.typicode.com/todos'
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users'
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
 
-employee_data = requests.get(user_url, {'id': employee_id})
-todo_data = requests.get(todo_url, {'userId': employee_id})
-completed_todo = requests.get(todo_url, {'userId': employee_id,
-                              'completed': 'true'})
-employee_name = employee_data.json()[0].get('name')
-done_todos = len(completed_todo.json())
-total_todo = len(todo_data.json())
+    employee_data = requests.get(user_url, {'id': employee_id})
+    todo_data = requests.get(todo_url, {'userId': employee_id})
+    completed_todo = requests.get(todo_url, {'userId': employee_id,
+                                  'completed': 'true'})
+    employee_name = employee_data.json()[0].get('name')
+    done_todos = len(completed_todo.json())
+    total_todo = len(todo_data.json())
 
-print(f'Employee {employee_name} is done with\
-tasks({done_todos}/{total_todo}):')
-for todo in completed_todo.json():
-    print(f"\t {todo['title']}")
+    print(f'Employee {employee_name} is done with\
+    tasks({done_todos}/{total_todo}):')
+    for todo in completed_todo.json():
+        print(f"\t {todo['title']}")
