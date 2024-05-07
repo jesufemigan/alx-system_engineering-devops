2f339a2de5d10cbffc27ddba128fe5c74250afe5
Jesufemi <oladapofemmy@gmail.com>
2024-05-07 20:56:49 +0100

    f0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     '''return number of subscribers'''
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent": "ubuntu:alx.api.advance.task"
+            "User-Agent": "ubuntu:alx.api.advance.task:v1.0.0 (by /u/hel_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
